df81fd42efe8b2dce41f1798870e804367a74f44
mohd-tanveer <tanveer18596@gmail.com>
2022-04-09 00:44:32 +0530

    storing the default schema

--- hackathonHackerearth/src/main/java/com/hackerearth/hackathonHackerearth/SpringSecurity/SecurityConfiguration.java
+++ hackathonHackerearth/src/main/java/com/hackerearth/hackathonHackerearth/SpringSecurity/SecurityConfiguration.java
@@ -1,17 +1,24 @@
 package com.hackerearth.hackathonHackerearth.SpringSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
+    @Autowired
+    DataSource dataSource;
+
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user")
@@ -22,6 +29,22 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .password("admin")
                 .roles("ADMIN");
 
+    }*/
+    //configure data source
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication() //got information and go to database
+            .dataSource(dataSource) //embeded database h2 autoconfigure
+            .withDefaultSchema()
+            .withUser(
+                    User.withUsername("user")
+                    .password("user")
+                    .roles("USER"))
+            .withUser(
+                    User.withUsername("admin")
+                    .password("admin")
+                    .roles("ADMIN")
+            );
     }
 
     @Bean
@@ -35,6 +58,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("USER","ADMIN")
                 .antMatchers("/home").permitAll() //for all
+                //.antMatchers("/**").permitAll() // for other urls
                 .and().formLogin();
     }
 }
